87d3232c497d6950fdcb639f49801d48aa310ff4
Jairo Andres Teran <jairoteran6@gmail.com>
2021-06-27 21:47:08 -0500

    Conexion a BD y handlers

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
-package twittor
+package main
+
+import (
+	"github.com/jairoteran6/twittor/bd"
+	"github.com/jairoteran6/twittor/handlers"
+	"log"
+)
 
 func main() {
-
+	if bd.ChequeoConnection()==0{
+		log.Fatal("Sin conexion a la BD")
+		return
+	}
+	handlers.Manejadores()
 }
